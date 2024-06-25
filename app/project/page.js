164fbe362ecb0f19a59e4520c12c86d7a64a90d2
clara7227 <clarasalgado27@gmail.com>
2024-06-25 12:40:32 +0200

    Página project finalizada

--- app/project/page.js
+++ app/project/page.js
@@ -63,46 +63,66 @@ export default function Projects() {
         </div>
       </section>
       <section className="subprojects standard_margin-y">
-        <h2 className="text-left standard_margin-x">{t("project.Subprojects.sectionTitle")}</h2>
-        <div className="standard_margin-x grid grid-cols-2 gap-32 subprojects_container">
-          <div className="text-ffdGreen-300">
-            <h3 className=""> {t("project.Subprojects.Subproject1.Title")}</h3>
-            <h5> {t("project.Subprojects.Subproject1.Subtitle")}</h5>
-            <p> {t("project.Subprojects.Subproject1.Body")} </p>
-            <button className="action_button margin_top_button text-ffdGreen-300 border-ffdGreen-300">
-              <Link rel="noopener noreferrer" target="_blank" href="#">
-                <span>{t("project.Subprojects.Subproject1.Button")}</span>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </Link>
-            </button>
+        <h2 className="text-left standard_margin-x mb-4 sm:mb-8 md:mb-12">
+          {t("project.Subprojects.sectionTitle")}
+        </h2>
+        <div className="flex flex-col sm:flex-row subprojects_container gap-0 sm:gap-16">
+          <div className="standard_margin-l subproject text-ffdGreen-300 basis-6/12 py-4 md:py-6 xl:py-8 border-4 border-solid border-transparent border-t-ffdGreen-400">
+         
+              <h3 className="">
+                {" "}
+                {t("project.Subprojects.Subproject1.Title")}
+              </h3>
+              <h5> {t("project.Subprojects.Subproject1.Subtitle")}</h5>
+              <p> {t("project.Subprojects.Subproject1.Body")} </p>
+              <button className="action_button margin_top_button text-ffdGreen-300 border-ffdGreen-300">
+                <Link rel="noopener noreferrer" target="_blank" href="#">
+                  <span>{t("project.Subprojects.Subproject1.Button")}</span>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </Link>
+              </button>
+      
           </div>
-          <div className="text-ffdBlue-300">
-          <h3 className=""> {t("project.Subprojects.Subproject2.Title")}</h3>
-            <h5> {t("project.Subprojects.Subproject2.Subtitle")}</h5>
-            <p> {t("project.Subprojects.Subproject2.Body")} </p>
-            <button className="action_button margin_top_button text-ffdBlue-300 border-ffdBlue-300">
-              <Link rel="noopener noreferrer" target="_blank" href="#">
-              <span>{t("project.Subprojects.Subproject2.Button")}</span>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </Link>
-            </button>
+          {/* <div className="divisor-container basis-2/12">
+            {/* <img
+              className=" divisor-partial"
+              src="assets/img/divisor-partial.png"
+            ></img> 
+          </div> */}
+          <div className="standard_margin-r subproject text-ffdBlue-300 basis-6/12 py-4 md:py-6 xl:py-8 border-4 border-solid border-transparent border-t-ffdBlue-300">
+           
+              <h3 className="">
+                {" "}
+                {t("project.Subprojects.Subproject2.Title")}
+              </h3>
+              <h5> {t("project.Subprojects.Subproject2.Subtitle")}</h5>
+              <p> {t("project.Subprojects.Subproject2.Body")} </p>
+              <button className="action_button margin_top_button text-ffdBlue-300 border-ffdBlue-300">
+                <Link rel="noopener noreferrer" target="_blank" href="#">
+                  <span>{t("project.Subprojects.Subproject2.Button")}</span>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </Link>
+              </button>
+           
           </div>
         </div>
       </section>
-      <section className="standard_margin">
+      <section className="highlight-section">
+        <div className="standard_padding">
         <div className="flex flex-col sm:gap-4 md:gap-4">
           <h2 className="text-left">{t("project.Impact.sectionTitle")}</h2>
-          <p className="sm:basis-3/4">{t("project.Impact.sectionBody")}</p>
+          <p className="sm:w-5/6">{t("project.Impact.sectionBody")}</p>
+        </div>
         </div>
       </section>
-      <div className="banner px-4 sm:px-8 md:px-14 md:py-2 lg:px-24 lg:py-4 xl:px-28 xl:py-4 2xl:px-32 2xl:py-6">
+      {/* <div className="banner px-4 sm:px-8 md:px-14 md:py-2 lg:px-24 lg:py-4 xl:px-28 xl:py-4 2xl:px-32 2xl:py-6">
         <h1> {t("projects.title")}</h1>
       </div>
       <main>
         <section className="lg:mx-36 md:mx-14 sm:mx-8 my-4 sm:my-4 md:my-8 lg:my-12 xl:my-16 2xl:my-20 mx-4 xl:mx-44 2xl:mx-60">
           <ProjectCard />
         </section>
-      </main>
+      </main> */}
       <Footer />
     </div>
   );
