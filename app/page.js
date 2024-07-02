e8dd201a14b3fb5646c3fe54f5d4747c47751dd5
sleepndraw <thepikahouse@gmail.com>
2024-07-02 15:04:28 +0200

    framer motion instalado y algunas pruebas de animaciones

--- app/page.js
+++ app/page.js
@@ -14,24 +14,32 @@ import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 import { useTranslation } from "react-i18next";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
   const [carousel, setCarousel] = useState(mycarousel);
   const { t, i18n } = useTranslation();
   const currentLang = i18n.language;
-
+  const [isHovered, setIsHovered] = useState(false);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div className="App">
+    <AnimatePresence>
+      
+        <div className="App">
       <div className={"home_page page_" + currentLang}>
         <Header route={"/"} />
         <div className="parallax_layer parallax__layer--base">
           <main>
             <section className="home">
-              <div className="head block sm:flex sm:flew-row items-center py-8 sm:py-12 md:py-20 lg:py-24 2xl:py-32">
+            <motion.div className="body"
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}>
+                                  <div className="head block sm:flex sm:flew-row items-center py-8 sm:py-12 md:py-20 lg:py-24 2xl:py-32">
                 <div className="group_description flex flex-col gap-4 sm:items-start md:gap-8  mx-8 sm:mx-8 md:ml-12 lg:ml-16 xl:ml-20 2xl:ml-24 sm:w-3/5">
                   <img
                     className="logo_home mt-4 sm:mt-0 self-start w-full"
@@ -39,30 +47,46 @@ export default function Home() {
                   />
                   <h3 className="self-start">{t("front.title")}</h3>
                   <div className="container_front_description">
-                  <p className="front_description ">{t("front.description")}</p>
-                  <button className="action_button self-start">
-                    <Link rel="noopener noreferrer" target="_blank" href="#">
-                      <span>{t("front.action-button")}</span>
-                      <FontAwesomeIcon icon={faArrowRight} />
-                    </Link>
-                  </button>
+                    <p className="front_description ">
+                      {t("front.description")}
+                    </p>
+                    <motion.button
+                      className="action_button self-start"
+                      onHoverStart={() => setIsHovered(true)}
+                      onHoverEnd={() => setIsHovered(false)}
+                    >
+                      <Link rel="noopener noreferrer" target="_blank" href="#">
+                        <span>{t("front.action-button")}</span>
+
+                        <motion.div
+                          animate={{ x: isHovered ? 10 : 0 }} // Adjust '10' to the desired amount of movement
+                          transition={{ type: "spring", stiffness: 300 }}
+                          style={{ display: "inline-block", marginLeft: "8px" }} // Adjust 'marginLeft' for spacing
+                        >
+                          <FontAwesomeIcon icon={faArrowRight} />
+                        </motion.div>
+                      </Link>
+                    </motion.button>
                   </div>
                 </div>
                 <div className="banner_f4d_img sm:mt-0 sm:w-3/5" />
               </div>
+                  </motion.div>
+
 
               <div className="body">
                 <section className="project_description">
                   <div className="xl_margin gap_text">
                     <div>
-                    <h2 className="text-left">{t("front.projectTitle")} </h2>
-                 
+                      <h2 className="text-left">{t("front.projectTitle")} </h2>
+
                       <h5 className="text-left sm:text-center">
                         {t("front.projectSubtitle")}{" "}
                       </h5>
-                      </div>
-                      <p className="text-white">{t("front.projectDescription")} </p>
-               
+                    </div>
+                    <p className="text-white">
+                      {t("front.projectDescription")}{" "}
+                    </p>
                   </div>
                 </section>
                 <section className="standard_margin">
@@ -91,5 +115,7 @@ export default function Home() {
         </div>
       </div>
     </div>
+    </AnimatePresence>
+
   );
 }
