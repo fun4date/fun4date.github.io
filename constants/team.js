54d374a42ba25c92a9637c3563c7d5a791aa31ba
Enrique <enrique.barra@upm.es>
2024-07-10 00:10:46 +0200

    small changes and first papers

--- constants/team.js
+++ constants/team.js
@@ -176,7 +176,7 @@ export const myteam = {
     members: [
       {
         name: "Pedro Reviriego",
-        position: "IP1",
+        position: "Profesor titular de universidad - IP1",
         translationKey: "team.professorCards.roles.2",
         description:
           "Pedro Reviriego received the M.Sc. and Ph.D. degrees in telecommunications engineering from the Technical University of Madrid, Madrid, Spain, in 1994 and 1997, respectively. From 1997 to 2000, he was an Engineer with Teldat, Madrid, working on router implementation. In 2000, he joined Massana to work on the development of 1000BASE-T transceivers. From 2004 to 2007, he was a Distinguished Member of Technical Staff with the LSI Corporation, working on the development of Ethernet transceivers. From 2007 to 2018 he was with Nebrija University, from 2018 to 2022 with Universidad Carlos III de Madrid. He is currently with Universidad Politécnica de Madrid working on probabilistic data structures, high speed packet processing and machine learning.",
@@ -200,7 +200,7 @@ export const myteam = {
       {
         name: "Enrique Barra",
         // position: "team.professorCards.positions.1",
-        position: "IP2",
+        position: "Profesor contratado doctor - IP2",
         translationKey: "team.professorCards.roles.2",
         description:
           "Enrique Barra is a professor at Telematic Engineering Department, School of Telecommunications Engineering. He received the Ph.D. degree in Telematic Engineering with minoring in Multimedia and Technology Enhanced Learning from  Universidad Politécnica de Madrid (UPM).He has participated in many European projects, such as GLOBAL, FIWARE, and C@R. He is currently involved in several projects contributing to the generation and distribution of educational content in TEL environments. His research interests include videoconferencing, games in education, and social networks in education.",
@@ -212,7 +212,7 @@ export const myteam = {
       },
       {
         name: "Álvaro Alonso",
-        position: "",
+        position: "Profesor contratado doctor",
         translationKey: "team.professorCards.roles.2",
         description:
           "Álvaro Alonso, Ph.D. in Telecommunications Engineering, Professor at the Telematic Systems Engineering Department, School of Telecommunications Engineering, is a WebHTML5 Videoconference and Cloud Computing expert.He is the author of several articles and participates in a lot of Spanish and European research projects. His research interests include multi-conferencing systems in cloud computing, security, and the IoT in the future Internet.",
@@ -224,6 +224,7 @@ export const myteam = {
       },
       {
         name: "Aldo Gordillo",
+        position: "Profesor contratado doctor",
         translationKey: "team.professorCards.roles.2",
         description:
           "Aldo Gordillo received the degree in Telecommunications Engineering and the PhD degree in Telematics Engineering from UPM, in 2012 and 2017, respectively. From 2012 to 2017, he was a Research and Development Engineer with the Telematics Engineering Department, UPM. He is currently an Assistant Professor with the Computer Science Department, also at UPM. His research interests include the field of technology-enhanced learning, with a special focus on creation, evaluation, and dissemination of e-learning resources, computer science education, game-based learning, and e-learning systems.",
@@ -235,7 +236,7 @@ export const myteam = {
       },
       {
         name: "Gabriel Huecas",
-        position: "",
+        position: "Profesor titular de universidad",
         translationKey: "team.professorCards.roles.2",
         description:
           "Gabriel Huecas, Ph.D in Telecommunications Engineering, is currently the Postgraduate Assistant Director at the School of Telecommunications Engineering of Universidad Politécnica de Madrid (UPM), as well as a professor at the same institution. He is the author of different Java 2 programming books.",
@@ -247,7 +248,7 @@ export const myteam = {
       },
       {
         name: "Joaquín Salvachúa",
-        position: "",
+        position: "Profesor titular de universidad",
         translationKey: "team.professorCards.roles.2",
         description:
           "Joaquín Salvachúa is a professor at UPM, where he received the M.Sc., Ph.D. in Telecommunications Engineering. He is also FIWARE Technical Steering Committee member elected as Remarkable individuals. His research interests include advanced cloud and edge architectures, big data infrastructure, data privacy and usage control, NoSql databases, applications, and identity in blockchain.",
@@ -260,7 +261,7 @@ export const myteam = {
       {
         name: "Santiago Pavón",
         translationKey: "team.professorCards.roles.2",
-        position: "",
+        position: "Profesor titular de universidad",
         description:
           "Santiago Pavón Gómez obtained the B.S. and Ph.D. in Telecommunication Engineer from the Universidad Politécnica de Madrid (UPM) in 1987 and 1990 respectively. Santiago obtained the position of Assistant Professor in 1991 and the position of Associate Professor in 1992. He has published several articles and participated in national and international research projects related to Formal Description Techniques, CSCW, Tele-Education, collaboration services, IPv6, virtualization, identity services, safely (Cyspa) and Blockchain (BAPI).",
         photo: "/assets/img/team/santiago-pavon.jpg",
@@ -271,7 +272,7 @@ export const myteam = {
       },
       {
         name: "Jose Andres Muñoz",
-        position: "",
+        position: "Profesor ayudante doctor",
         translationKey: "team.professorCards.roles.3",
         description:
           "Andrés received the B.S. in Telematic Engineering from Escuela Superior Politécnica del Litoral (ESPOL) in 2013 He received the M.Sc. in Telematic Services and Network Engineering from Universidad Politécnica de Madrid (UPM). Andrés is currently a Ph.D. student in Telecommunications Engineering and a Researcher with the Next Generation Internet Research Group. His previous experience includes Telematic Laboratories Coordination or Servers and Communications Assistant, both in ESPOL.Andres has carried out different research projects, such as Web Platform Development for Control and Monitoring of Environmental Parameters in Agriculture or Kiswar Proyect Reactive Evacuation Management Systemsfor Emergency Situations.",
@@ -283,7 +284,7 @@ export const myteam = {
       },
       {
         name: "Alejandro Pozo",
-        position: "",
+        position: "Profesor ayudante doctor",
         translationKey: "team.professorCards.roles.3",
         description:
           "Researcher and Ph.D. student at Politécnica of Madrid (UPM). He received the M.Sc. in Telecommunications Engineering from the UPM, Spain, in 2017, where he is currently pursuing the Ph.D. in Telecommunications Engineering and is a Research Assistant with the Next Generation Internet Research Group. He participates in several European projects and publishes results in journals related to the Internet of Things, security, and identity management.",
@@ -295,7 +296,7 @@ export const myteam = {
       },
       {
         name: "Anabel Pilicita",
-        position: "PhD Candidate",
+        position: "Estudiante de doctorado",
         translationKey: "",
         description: "",
         photo: "/assets/img/team/avatar.png",
@@ -306,7 +307,7 @@ export const myteam = {
       },
       {
         name: "Carlos Arriaga",
-        position: "PhD Candidate",
+        position: "Estudiante de doctorado",
         translationKey: "team.professorCards.roles.4",
         description:
           " Carlos is a Ph.D. student in Telecommunication Engineering at Universidad Politécnica de Madrid (UPM).  He received the B.S. and M.Sc. in Telecommunications Engineering from UPM in 2020 and 2022. His research interests lie in primarily in the field of video-conference scalability and WebRTC.",
@@ -318,7 +319,7 @@ export const myteam = {
       },
       {
         name: "Javier Conde",
-        position: "PhD Candidate",
+        position: "Profesor ayudante doctor",
         translationKey: "team.professorCards.roles.3",
         description:
           "PhD student in Telematics Engineering at UPM and researcher in the Department of Telematics Engineering. He received the B.S. and M.Sc. in Telecommunications Engineering from UPM in 2018 and 2020. His research interests lie in the fields of Open Linked Data, Digital Twins, Big Data and Machine Learning.",
@@ -330,7 +331,7 @@ export const myteam = {
       },
       {
         name: "Carlos Aparicio",
-        position: "PhD Candidate",
+        position: "Estudiante de doctorado",
         translationKey: "",
         description: "",
         photo: "/assets/img/team/avatar.png",
@@ -341,7 +342,7 @@ export const myteam = {
       },
       {
         name: "Elena Merino",
-        position: "Extern",
+        position: "Miembro externo del proyecto",
         translationKey: "",
         description: "",
         photo: "/assets/img/team/avatar.png",
@@ -352,7 +353,7 @@ export const myteam = {
       },
       {
         name: "Fernando Moral",
-        position: "Extern",
+        position: "Miembro externo del proyecto",
         translationKey: "",
         description: "",
         photo: "/assets/img/team/avatar.png",
@@ -363,7 +364,7 @@ export const myteam = {
       },
       {
         name: "Sonsoles López-Pernas",
-        position: "Extern",
+        position: "Miembro externo del proyecto",
         translationKey: "",
         description: "",
         photo: "/assets/img/team/avatar.png",
@@ -374,7 +375,7 @@ export const myteam = {
       },
       {
         name: "Mohammed Saqr",
-        position: "Extern",
+        position: "Miembro externo del proyecto",
         translationKey: "",
         description: "",
         photo: "/assets/img/team/avatar.png",
